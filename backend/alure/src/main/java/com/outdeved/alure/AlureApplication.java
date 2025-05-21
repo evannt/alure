8c0d69055cfd0a68c75eb531080c9c863df8157e
unknown <iamevannt@gmail.com>
2025-05-21 19:22:28 -0400

    Initial Commit

---
+++ backend/alure/src/main/java/com/outdeved/alure/AlureApplication.java
@@ -0,0 +1,13 @@
+package com.outdeved.alure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlureApplication.class, args);
+	}
+
+}
---
+++ backend/alure/src/test/java/com/outdeved/alure/AlureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outdeved.alure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
